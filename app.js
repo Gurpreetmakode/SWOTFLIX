4c724dd6855a7b0afc874c383892e7769a973c3f
Gurpreet Singh Makode <gurpreetsinghmakode@gmail.com>
2026-09-15 20:40:43 +0200

    Update team profile display name

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const profileImages = {
 };
 
 function getProfileDisplayName(key) {
-  if (key === 'team') return 'Team Profile';
+  if (key === 'team') return 'The Team';
   return swotData[key].name;
 }
 
